9e9a6d6a83dbd1d657fd6595f5a9b0497be7f1d4
yavuzgedik <yavuzgedik@outlook.com>
2016-12-10 16:43:48 +0200

    FakePaint

---
+++ FakePaint/FakePaint/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace FakePaint
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.RankSecBtn = new System.Windows.Forms.Button();
+            this.TemizleBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Kalınlık";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "8",
+            "16",
+            "32",
+            "64",
+            "128"});
+            this.comboBox1.Location = new System.Drawing.Point(3, 16);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 1;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // RankSecBtn
+            // 
+            this.RankSecBtn.Location = new System.Drawing.Point(130, 14);
+            this.RankSecBtn.Name = "RankSecBtn";
+            this.RankSecBtn.Size = new System.Drawing.Size(75, 23);
+            this.RankSecBtn.TabIndex = 2;
+            this.RankSecBtn.Text = "Renk Seç";
+            this.RankSecBtn.UseVisualStyleBackColor = true;
+            this.RankSecBtn.Click += new System.EventHandler(this.RankSecBtn_Click);
+            // 
+            // TemizleBtn
+            // 
+            this.TemizleBtn.Location = new System.Drawing.Point(211, 14);
+            this.TemizleBtn.Name = "TemizleBtn";
+            this.TemizleBtn.Size = new System.Drawing.Size(75, 23);
+            this.TemizleBtn.TabIndex = 3;
+            this.TemizleBtn.Text = "Temizle";
+            this.TemizleBtn.UseVisualStyleBackColor = true;
+            this.TemizleBtn.Click += new System.EventHandler(this.TemizleBtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(304, 135);
+            this.Controls.Add(this.TemizleBtn);
+            this.Controls.Add(this.RankSecBtn);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseUp);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button RankSecBtn;
+        private System.Windows.Forms.Button TemizleBtn;
+    }
+}
+
---
+++ FakePaint/FakePaint/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FakePaint
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Graphics g;
+        ColorDialog colorDialog = new ColorDialog();
+        int kalinlik = 3;
+        int baslaX, baslaY;
+        bool ciz;
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            g = CreateGraphics();
+            Pen p = new Pen(colorDialog.Color, kalinlik);
+
+            Point point1 = new Point(baslaX, baslaY);
+            Point point2 = new Point(e.X, e.Y);
+
+            if (ciz == true)
+            {
+                g.DrawLine(p, point1, point2);
+                baslaX = e.X;
+                baslaY = e.Y;
+            }
+        }
+
+        private void RankSecBtn_Click(object sender, EventArgs e)
+        {
+            colorDialog.ShowDialog();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            kalinlik = int.Parse(comboBox1.SelectedItem.ToString());
+        }
+
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        {
+            ciz = false;
+        }
+
+        private void TemizleBtn_Click(object sender, EventArgs e)
+        {
+            this.Invalidate();
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            ciz = true;
+            baslaX = e.X;
+            baslaY = e.Y;
+        }
+    }
+}
