f0959bfa00f166b23e4f146747475e2b0c6a02d3
yavuzgedik <yavuzgedik@outlook.com>
2016-12-10 18:05:03 +0200

    Updated

---
+++ FormlarArasıGecis/FormlarArasıGecis/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FormlarArasıGecis
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Form2 frm = new Form2();
+        private void button1_Click(object sender, EventArgs e)
+        {
+            frm.mesaj = textBox1.Text;
+            frm.Show();
+            this.Hide();
+        }
+    }
+}
---
+++ FormlarArasıGecis/FormlarArasıGecis/Form2.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FormlarArasıGecis
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+        public string mesaj;
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            //MessageBox.Show(mesaj);
+            label1.Text = "Merhaba Dostum " + mesaj; 
+        }
+    }
+}
