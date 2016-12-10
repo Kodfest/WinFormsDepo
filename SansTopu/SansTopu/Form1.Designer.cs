b6e32d1d96be819aba4a06a8d289f52f29f0f51b
yavuzgedik <yavuzgedik@outlook.com>
2016-12-10 14:48:15 +0200

    Updated SansTopu

--- SansTopu/SansTopu/Form1.Designer.cs
+++ SansTopu/SansTopu/Form1.Designer.cs
@@ -111,7 +111,6 @@
             // 
             // timer1
             // 
-            this.timer1.Interval = 10;
             this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
             // Form1
--- SansTopu/SansTopu/Form1.cs
+++ SansTopu/SansTopu/Form1.cs
@@ -25,35 +25,30 @@ namespace SansTopu
         Random rnd = new Random();
         int rasgeleSayi;
         List<int> Sayilar = new List<int>(6);
-
-        //int[] Sayilar = new int[6];
+        
         int sayac = 0;
         int turSayisi = 1;
-
         private void timer1_Tick(object sender, EventArgs e)
         {
-            if (turSayisi % 4 == 0)
+            if (turSayisi % 10 == 0)
             {
                 timer1.Stop();
-                //Sayilar.Sort(); // Sort işlem bitince, yani son çıkan sayıları sıralıyor
             }
             else
             {
-                Sayilar = new List<int>(6);
-
+                Sayilar.Clear();
 
                 while (sayac < 6)
                 {
                     rasgeleSayi = rnd.Next(1, 50);
                     if (!Sayilar.Contains(rasgeleSayi))
                     {
-                        //Sayilar[sayac] = rasgeleSayi;
                         Sayilar.Add(rasgeleSayi);
                         sayac++;
                     }
                 }
 
-                Sayilar.Sort(); // Burada şuan anlamadığımız bir hata veriyor, araştıracağız!
+                Sayilar.Sort();
 
                 label1.Text = Sayilar[0].ToString();
                 label2.Text = Sayilar[1].ToString();
@@ -61,12 +56,10 @@ namespace SansTopu
                 label4.Text = Sayilar[3].ToString();
                 label5.Text = Sayilar[4].ToString();
                 label6.Text = Sayilar[5].ToString();
-
             }
 
             sayac = 0;
             turSayisi++;
-
         }
     }
 }
