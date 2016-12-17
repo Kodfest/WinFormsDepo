d32334feb7eb3b53c1638a16dc35dd48683942a6
yavuzgedik <yavuzgedik@outlook.com>
2016-12-17 18:08:13 +0200

    Otobus

---
+++ SeckinTruzim/SeckinTruzim/Form1.Designer.cs
@@ -0,0 +1,268 @@
+﻿namespace SeckinTruzim
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.CBoxOtobusTuru = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.PanelTravego = new System.Windows.Forms.Panel();
+            this.PanelNeoplan = new System.Windows.Forms.Panel();
+            this.GBoxMusteriBilgileri = new System.Windows.Forms.GroupBox();
+            this.LabelOtobusTuru = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.LabelKoltukNo = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.GBoxMusteriCinsiyet = new System.Windows.Forms.GroupBox();
+            this.RadioBtnErkek = new System.Windows.Forms.RadioButton();
+            this.RadioBtnKadin = new System.Windows.Forms.RadioButton();
+            this.BtnKaydet = new System.Windows.Forms.Button();
+            this.TxtMusteriAdSoyAd = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.GBoxMusteriBilgileri.SuspendLayout();
+            this.GBoxMusteriCinsiyet.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.pictureBox1.Location = new System.Drawing.Point(16, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(256, 115);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // CBoxOtobusTuru
+            // 
+            this.CBoxOtobusTuru.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.CBoxOtobusTuru.FormattingEnabled = true;
+            this.CBoxOtobusTuru.Items.AddRange(new object[] {
+            "Travego",
+            "Neoplan"});
+            this.CBoxOtobusTuru.Location = new System.Drawing.Point(12, 181);
+            this.CBoxOtobusTuru.Name = "CBoxOtobusTuru";
+            this.CBoxOtobusTuru.Size = new System.Drawing.Size(256, 28);
+            this.CBoxOtobusTuru.TabIndex = 1;
+            this.CBoxOtobusTuru.SelectedIndexChanged += new System.EventHandler(this.CBoxOtobusTuru_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(8, 158);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(113, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Otobüs Türü:";
+            // 
+            // PanelTravego
+            // 
+            this.PanelTravego.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.PanelTravego.Location = new System.Drawing.Point(291, 12);
+            this.PanelTravego.Name = "PanelTravego";
+            this.PanelTravego.Size = new System.Drawing.Size(155, 365);
+            this.PanelTravego.TabIndex = 3;
+            // 
+            // PanelNeoplan
+            // 
+            this.PanelNeoplan.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.PanelNeoplan.Location = new System.Drawing.Point(290, 13);
+            this.PanelNeoplan.Name = "PanelNeoplan";
+            this.PanelNeoplan.Size = new System.Drawing.Size(155, 425);
+            this.PanelNeoplan.TabIndex = 4;
+            // 
+            // GBoxMusteriBilgileri
+            // 
+            this.GBoxMusteriBilgileri.Controls.Add(this.LabelOtobusTuru);
+            this.GBoxMusteriBilgileri.Controls.Add(this.label5);
+            this.GBoxMusteriBilgileri.Controls.Add(this.LabelKoltukNo);
+            this.GBoxMusteriBilgileri.Controls.Add(this.label3);
+            this.GBoxMusteriBilgileri.Controls.Add(this.GBoxMusteriCinsiyet);
+            this.GBoxMusteriBilgileri.Controls.Add(this.BtnKaydet);
+            this.GBoxMusteriBilgileri.Controls.Add(this.TxtMusteriAdSoyAd);
+            this.GBoxMusteriBilgileri.Controls.Add(this.label2);
+            this.GBoxMusteriBilgileri.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.GBoxMusteriBilgileri.Location = new System.Drawing.Point(465, 13);
+            this.GBoxMusteriBilgileri.Name = "GBoxMusteriBilgileri";
+            this.GBoxMusteriBilgileri.Size = new System.Drawing.Size(259, 234);
+            this.GBoxMusteriBilgileri.TabIndex = 5;
+            this.GBoxMusteriBilgileri.TabStop = false;
+            this.GBoxMusteriBilgileri.Text = "Müşteri Bilgileri";
+            // 
+            // LabelOtobusTuru
+            // 
+            this.LabelOtobusTuru.AutoSize = true;
+            this.LabelOtobusTuru.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.LabelOtobusTuru.Location = new System.Drawing.Point(129, 168);
+            this.LabelOtobusTuru.Name = "LabelOtobusTuru";
+            this.LabelOtobusTuru.Size = new System.Drawing.Size(13, 17);
+            this.LabelOtobusTuru.TabIndex = 7;
+            this.LabelOtobusTuru.Text = "-";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.Location = new System.Drawing.Point(20, 168);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(109, 17);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Otobüs Türü :";
+            // 
+            // LabelKoltukNo
+            // 
+            this.LabelKoltukNo.AutoSize = true;
+            this.LabelKoltukNo.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.LabelKoltukNo.Location = new System.Drawing.Point(110, 145);
+            this.LabelKoltukNo.Name = "LabelKoltukNo";
+            this.LabelKoltukNo.Size = new System.Drawing.Size(16, 17);
+            this.LabelKoltukNo.TabIndex = 5;
+            this.LabelKoltukNo.Text = "0";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(20, 145);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(88, 17);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Koltuk No :";
+            // 
+            // GBoxMusteriCinsiyet
+            // 
+            this.GBoxMusteriCinsiyet.Controls.Add(this.RadioBtnErkek);
+            this.GBoxMusteriCinsiyet.Controls.Add(this.RadioBtnKadin);
+            this.GBoxMusteriCinsiyet.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.GBoxMusteriCinsiyet.Location = new System.Drawing.Point(17, 69);
+            this.GBoxMusteriCinsiyet.Name = "GBoxMusteriCinsiyet";
+            this.GBoxMusteriCinsiyet.Size = new System.Drawing.Size(236, 68);
+            this.GBoxMusteriCinsiyet.TabIndex = 3;
+            this.GBoxMusteriCinsiyet.TabStop = false;
+            this.GBoxMusteriCinsiyet.Text = "Cinsiyet";
+            // 
+            // RadioBtnErkek
+            // 
+            this.RadioBtnErkek.AutoSize = true;
+            this.RadioBtnErkek.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.RadioBtnErkek.Location = new System.Drawing.Point(7, 41);
+            this.RadioBtnErkek.Name = "RadioBtnErkek";
+            this.RadioBtnErkek.Size = new System.Drawing.Size(62, 21);
+            this.RadioBtnErkek.TabIndex = 1;
+            this.RadioBtnErkek.TabStop = true;
+            this.RadioBtnErkek.Text = "Erkek";
+            this.RadioBtnErkek.UseVisualStyleBackColor = true;
+            // 
+            // RadioBtnKadin
+            // 
+            this.RadioBtnKadin.AutoSize = true;
+            this.RadioBtnKadin.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.RadioBtnKadin.Location = new System.Drawing.Point(7, 18);
+            this.RadioBtnKadin.Name = "RadioBtnKadin";
+            this.RadioBtnKadin.Size = new System.Drawing.Size(62, 21);
+            this.RadioBtnKadin.TabIndex = 0;
+            this.RadioBtnKadin.TabStop = true;
+            this.RadioBtnKadin.Text = "Kadın";
+            this.RadioBtnKadin.UseVisualStyleBackColor = true;
+            // 
+            // BtnKaydet
+            // 
+            this.BtnKaydet.BackColor = System.Drawing.Color.DarkGreen;
+            this.BtnKaydet.ForeColor = System.Drawing.Color.White;
+            this.BtnKaydet.Location = new System.Drawing.Point(17, 193);
+            this.BtnKaydet.Name = "BtnKaydet";
+            this.BtnKaydet.Size = new System.Drawing.Size(236, 35);
+            this.BtnKaydet.TabIndex = 2;
+            this.BtnKaydet.Text = "KAYDET";
+            this.BtnKaydet.UseVisualStyleBackColor = false;
+            this.BtnKaydet.Click += new System.EventHandler(this.BtnKaydet_Click);
+            // 
+            // TxtMusteriAdSoyAd
+            // 
+            this.TxtMusteriAdSoyAd.Location = new System.Drawing.Point(102, 29);
+            this.TxtMusteriAdSoyAd.Name = "TxtMusteriAdSoyAd";
+            this.TxtMusteriAdSoyAd.Size = new System.Drawing.Size(151, 26);
+            this.TxtMusteriAdSoyAd.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(14, 38);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(82, 17);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Ad Soyad:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(736, 476);
+            this.Controls.Add(this.GBoxMusteriBilgileri);
+            this.Controls.Add(this.PanelNeoplan);
+            this.Controls.Add(this.PanelTravego);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.CBoxOtobusTuru);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.GBoxMusteriBilgileri.ResumeLayout(false);
+            this.GBoxMusteriBilgileri.PerformLayout();
+            this.GBoxMusteriCinsiyet.ResumeLayout(false);
+            this.GBoxMusteriCinsiyet.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ComboBox CBoxOtobusTuru;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel PanelTravego;
+        private System.Windows.Forms.Panel PanelNeoplan;
+        private System.Windows.Forms.GroupBox GBoxMusteriBilgileri;
+        private System.Windows.Forms.Label LabelOtobusTuru;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label LabelKoltukNo;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.GroupBox GBoxMusteriCinsiyet;
+        private System.Windows.Forms.RadioButton RadioBtnErkek;
+        private System.Windows.Forms.RadioButton RadioBtnKadin;
+        private System.Windows.Forms.Button BtnKaydet;
+        private System.Windows.Forms.TextBox TxtMusteriAdSoyAd;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ SeckinTruzim/SeckinTruzim/Form1.cs
@@ -0,0 +1,249 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SeckinTruzim
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            pictureBox1.ImageLocation = System.IO.Path.GetFullPath("Photos/pt_logo.png");
+           
+            this.BackColor = Color.FromArgb(255, 255, 255);
+
+            PanelTravego.Visible = false;
+            PanelNeoplan.Visible = false;
+
+            GBoxMusteriBilgileri.Enabled = false;
+
+            #region TavegoKoltuklari
+            byte counter = 1;
+            for (int i = 0; i < 12; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    if ((j != 2 || i == 11) && (i != 5 || j < 2))
+                    {
+                        Button btn = new Button();
+                        btn.Click += ButtonClick;
+                        btn.Width = 30;
+                        btn.Height = 30;
+                        btn.Text = counter + ""; //counter.ToString();
+                        btn.BackColor = RenkOlustur(counter, "Travego");
+                        btn.ForeColor = Color.White;
+                        btn.Left = btn.Width * j;
+                        btn.Top = btn.Height * i;
+                        PanelTravego.Controls.Add(btn);
+                        counter++;
+                    }
+                }
+            }
+            #endregion
+
+            #region NeoplanKoltuklari
+            counter = 1;
+            for (int i = 0; i < 14; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    if ((j != 2 || i == 13) && (i != 6 || j < 2))
+                    {
+                        Button btn = new Button();
+                        btn.Click += ButtonClick;
+                        btn.Width = 30;
+                        btn.Height = 30;
+                        btn.Text = counter + ""; //counter.ToString();
+                        btn.BackColor = RenkOlustur(counter, "Neoplan");
+                        btn.ForeColor = Color.White;
+                        btn.Left = btn.Width * j;
+                        btn.Top = btn.Height * i;
+                        PanelNeoplan.Controls.Add(btn);
+                        counter++;
+                    }
+                }
+            }
+            #endregion
+        }
+
+        private void CBoxOtobusTuru_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            TxtMusteriAdSoyAd.Clear(); // TxtMusteriAdSoyAd.Text = "";
+            RadioBtnErkek.Checked = false;
+            RadioBtnKadin.Checked = false;
+
+            if (CBoxOtobusTuru.SelectedItem.ToString() == "Neoplan")
+            {
+                PanelNeoplan.Visible = true;
+                PanelTravego.Visible = false;
+            }
+            else if (CBoxOtobusTuru.SelectedItem.ToString() == "Travego")
+            {
+                PanelTravego.Visible = true;
+                PanelNeoplan.Visible = false;
+            }
+        }
+
+        private void ButtonClick(object sender, EventArgs e)
+        {
+            
+            Button secilenKoltuk = sender as Button;
+            LabelKoltukNo.Text = secilenKoltuk.Text;
+            LabelOtobusTuru.Text = CBoxOtobusTuru.SelectedItem.ToString();
+
+            GBoxMusteriBilgileri.Enabled = true;
+
+            if (CBoxOtobusTuru.SelectedItem.ToString() == "Travego")
+            {
+                string yolcu = travegoYolcularIsim[int.Parse(secilenKoltuk.Text) - 1];
+
+                if (yolcu != "")
+                {
+                    TxtMusteriAdSoyAd.Text = yolcu;
+
+                    string cinsiyet = travegoYolcularCinsiyet[int.Parse(secilenKoltuk.Text) - 1];
+
+                    if (cinsiyet == "Erkek")
+                    {
+                        RadioBtnErkek.Checked = true;
+                        secilenKoltuk.BackColor = Color.Blue;
+                    }
+                    else if (cinsiyet == "Kadın")
+                    {
+                        RadioBtnKadin.Checked = true;
+                        secilenKoltuk.BackColor = Color.Pink;
+                    }
+                }
+            }
+            else
+            {
+                string yolcu = neoplanYolcularIsim[int.Parse(secilenKoltuk.Text) - 1];
+
+                if (yolcu != "")
+                {
+                    TxtMusteriAdSoyAd.Text = yolcu;
+
+                    string cinsiyet = neoplanYolcularCinsiyet[int.Parse(secilenKoltuk.Text) - 1];
+
+                    if (cinsiyet == "Erkek")
+                    {
+                        RadioBtnErkek.Checked = true;
+                        secilenKoltuk.BackColor = Color.Blue;
+                    }
+                    else if (cinsiyet == "Kadın")
+                    {
+                        RadioBtnKadin.Checked = true;
+                        secilenKoltuk.BackColor = Color.Pink;
+                    }
+                }
+            }
+        }
+
+        string[] travegoYolcularIsim = new string[47];
+        string[] travegoYolcularCinsiyet = new string[47];
+        string[] neoplanYolcularIsim = new string[55];
+        string[] neoplanYolcularCinsiyet = new string[55];
+        
+        private void BtnKaydet_Click(object sender, EventArgs e)
+        {
+            #region Kaydet
+            if (LabelOtobusTuru.Text == "Neoplan")
+            {
+                neoplanYolcularIsim[int.Parse(LabelKoltukNo.Text) - 1] = TxtMusteriAdSoyAd.Text;
+
+                string cinsiyet;
+
+                if (RadioBtnKadin.Checked)
+                {
+                    cinsiyet = "Kadın";
+                }
+                else
+                {
+                    cinsiyet = "Erkek";
+                }
+
+                neoplanYolcularCinsiyet[int.Parse(LabelKoltukNo.Text) - 1] = cinsiyet;
+                
+            }
+            else
+            {
+                travegoYolcularIsim[int.Parse(LabelKoltukNo.Text) - 1] = TxtMusteriAdSoyAd.Text;
+
+                string cinsiyet;
+
+                if (RadioBtnKadin.Checked)
+                {
+                    cinsiyet = "Kadın";
+                }
+                else
+                {
+                    cinsiyet = "Erkek";
+                }
+
+                travegoYolcularCinsiyet[int.Parse(LabelKoltukNo.Text) - 1] = cinsiyet;
+            }
+            #endregion
+            
+
+            PanelNeoplan.Controls.Clear();
+            PanelTravego.Controls.Clear();
+
+            Form1_Load(sender, e);
+
+            CBoxOtobusTuru.SelectedItem = LabelOtobusTuru.Text;
+            CBoxOtobusTuru_SelectedIndexChanged(sender, e);
+
+            TxtMusteriAdSoyAd.Clear();
+            LabelKoltukNo.Text = "";
+            LabelOtobusTuru.Text = "";
+        }
+
+        private Color RenkOlustur(int count, string otobusTuru)
+        {
+            Color color;
+            if(otobusTuru == "Travego")
+            {
+                string cinsiyet = travegoYolcularCinsiyet[count - 1];
+                if (cinsiyet == "Erkek")
+                {
+                    color = Color.Blue;
+                }
+                else if (cinsiyet == "Kadın")
+                {
+                    color = Color.Pink;
+                }
+                else
+                {
+                    color = Color.FromArgb(1, 22, 64);
+                }
+            }
+            else
+            {
+                string cinsiyet = neoplanYolcularCinsiyet[count - 1];
+                if (cinsiyet == "Erkek")
+                {
+                    color = Color.Blue;
+                }
+                else if (cinsiyet == "Kadın")
+                {
+                    color = Color.Pink;
+                }
+                else
+                {
+                    color = Color.FromArgb(1, 22, 64);
+                }
+            }
+            return color;
+        }
+    }
+}
