8dcd38fcf1b064d59d24ef1dc807b2fb5e0c2895
yavuzgedik <yavuzgedik@outlook.com>
2016-12-19 11:56:16 +0200

    Updated

---
+++ DialogMesaj/DialogMesaj/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DialogMesaj
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DialogResult result = MessageBox.Show("Devam Etmek İstiyor Musun?", "Onay Ekranı", MessageBoxButtons.YesNoCancel);
+
+            if (result == DialogResult.Yes)
+            {
+                button1.Text = "Evet";
+            }
+            else if (result == DialogResult.No)
+            {
+                button1.Text = "Hayır";
+            }
+            else
+            {
+                button1.Text = "Cancel";
+            }
+        }
+    }
+}
