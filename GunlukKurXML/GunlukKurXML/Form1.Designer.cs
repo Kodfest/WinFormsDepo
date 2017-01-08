d440d03d757f408b65a148186237da037b3a2ac3
yavuzgedik <yavuzgedik@outlook.com>
2017-01-08 16:50:38 +0200

    Added XML_KUR

---
+++ GunlukKurXML/GunlukKurXML/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace GunlukKurXML
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BtnGetir = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.DGridKurlar = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.DGridKurlar)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // BtnGetir
+            // 
+            this.BtnGetir.Location = new System.Drawing.Point(32, 32);
+            this.BtnGetir.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.BtnGetir.Name = "BtnGetir";
+            this.BtnGetir.Size = new System.Drawing.Size(100, 28);
+            this.BtnGetir.TabIndex = 0;
+            this.BtnGetir.Text = "GETİR";
+            this.BtnGetir.UseVisualStyleBackColor = true;
+            this.BtnGetir.Click += new System.EventHandler(this.BtnGetir_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(28, 11);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
+            // DGridKurlar
+            // 
+            this.DGridKurlar.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DGridKurlar.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2});
+            this.DGridKurlar.Location = new System.Drawing.Point(140, 32);
+            this.DGridKurlar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.DGridKurlar.Name = "DGridKurlar";
+            this.DGridKurlar.Size = new System.Drawing.Size(244, 148);
+            this.DGridKurlar.TabIndex = 2;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Para Birimi";
+            this.Column1.Name = "Column1";
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Kur Değeri";
+            this.Column2.Name = "Column2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(391, 190);
+            this.Controls.Add(this.DGridKurlar);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.BtnGetir);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.DGridKurlar)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BtnGetir;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView DGridKurlar;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+    }
+}
+
---
+++ GunlukKurXML/GunlukKurXML/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace GunlukKurXML
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnGetir_Click(object sender, EventArgs e)
+        {
+            XmlDocument xmlDoc = new XmlDocument();
+            xmlDoc.Load("http://www.tcmb.gov.tr/kurlar/today.xml");
+            DateTime tarih = Convert.ToDateTime(xmlDoc.SelectSingleNode("//Tarih_Date").Attributes["Tarih"].Value);
+
+            label1.Text = tarih.ToString("dd/MM/yyyy");
+
+            string USD = xmlDoc.SelectSingleNode("Tarih_Date/Currency[@Kod='USD']/BanknoteSelling").InnerXml;
+
+            string EUR = xmlDoc.SelectSingleNode("Tarih_Date/Currency[@Kod='EUR']/BanknoteSelling").InnerXml;
+
+            string GBP = xmlDoc.SelectSingleNode("Tarih_Date/Currency[@Kod='GBP']/BanknoteSelling").InnerXml;
+
+            DGridKurlar.Rows.Add("Dolar", USD);
+            DGridKurlar.Rows.Add("Euro", EUR);
+            DGridKurlar.Rows.Add("Pound", GBP);
+        }
+    }
+}
