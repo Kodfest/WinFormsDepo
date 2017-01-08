e88a27cc5d09fbbdd20a65436bd553cbf00f7e3b
yavuzgedik <yavuzgedik@outlook.com>
2017-01-08 17:36:49 +0200

    Updated

--- GunlukKurXML/GunlukKurXML/Form1.Designer.cs
+++ GunlukKurXML/GunlukKurXML/Form1.Designer.cs
@@ -33,15 +33,16 @@
             this.DGridKurlar = new System.Windows.Forms.DataGridView();
             this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.CBoxKurTuru = new System.Windows.Forms.ComboBox();
             ((System.ComponentModel.ISupportInitialize)(this.DGridKurlar)).BeginInit();
             this.SuspendLayout();
             // 
             // BtnGetir
             // 
-            this.BtnGetir.Location = new System.Drawing.Point(32, 32);
-            this.BtnGetir.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.BtnGetir.Location = new System.Drawing.Point(9, 40);
+            this.BtnGetir.Margin = new System.Windows.Forms.Padding(4);
             this.BtnGetir.Name = "BtnGetir";
-            this.BtnGetir.Size = new System.Drawing.Size(100, 28);
+            this.BtnGetir.Size = new System.Drawing.Size(121, 28);
             this.BtnGetir.TabIndex = 0;
             this.BtnGetir.Text = "GETİR";
             this.BtnGetir.UseVisualStyleBackColor = true;
@@ -50,7 +51,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(28, 11);
+            this.label1.Location = new System.Drawing.Point(137, 9);
             this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(46, 17);
@@ -64,7 +65,7 @@
             this.Column1,
             this.Column2});
             this.DGridKurlar.Location = new System.Drawing.Point(140, 32);
-            this.DGridKurlar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.DGridKurlar.Margin = new System.Windows.Forms.Padding(4);
             this.DGridKurlar.Name = "DGridKurlar";
             this.DGridKurlar.Size = new System.Drawing.Size(244, 148);
             this.DGridKurlar.TabIndex = 2;
@@ -73,24 +74,37 @@
             // 
             this.Column1.HeaderText = "Para Birimi";
             this.Column1.Name = "Column1";
+            this.Column1.ReadOnly = true;
             // 
             // Column2
             // 
             this.Column2.HeaderText = "Kur Değeri";
             this.Column2.Name = "Column2";
+            this.Column2.ReadOnly = true;
+            // 
+            // CBoxKurTuru
+            // 
+            this.CBoxKurTuru.FormattingEnabled = true;
+            this.CBoxKurTuru.Location = new System.Drawing.Point(9, 9);
+            this.CBoxKurTuru.Name = "CBoxKurTuru";
+            this.CBoxKurTuru.Size = new System.Drawing.Size(121, 24);
+            this.CBoxKurTuru.TabIndex = 3;
+            this.CBoxKurTuru.SelectedIndexChanged += new System.EventHandler(this.CboxSelected);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(391, 190);
+            this.Controls.Add(this.CBoxKurTuru);
             this.Controls.Add(this.DGridKurlar);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.BtnGetir);
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.DGridKurlar)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -104,6 +118,7 @@
         private System.Windows.Forms.DataGridView DGridKurlar;
         private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
         private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.ComboBox CBoxKurTuru;
     }
 }
 
--- GunlukKurXML/GunlukKurXML/Form1.cs
+++ GunlukKurXML/GunlukKurXML/Form1.cs
@@ -18,6 +18,7 @@ namespace GunlukKurXML
             InitializeComponent();
         }
 
+        bool dolarState = false, euroState = false, poundState = false;
         private void BtnGetir_Click(object sender, EventArgs e)
         {
             XmlDocument xmlDoc = new XmlDocument();
@@ -26,15 +27,80 @@ namespace GunlukKurXML
 
             label1.Text = tarih.ToString("dd/MM/yyyy");
 
-            string USD = xmlDoc.SelectSingleNode("Tarih_Date/Currency[@Kod='USD']/BanknoteSelling").InnerXml;
+            #region KurDegeriGetir
+            if (CBoxKurTuru.SelectedItem != null)
+            {
+                if (CBoxKurTuru.SelectedItem.ToString() == "Dolar" && !dolarState)
+                {
+                    string USD = xmlDoc.SelectSingleNode("Tarih_Date/Currency[@Kod='USD']/BanknoteSelling").InnerXml;
 
-            string EUR = xmlDoc.SelectSingleNode("Tarih_Date/Currency[@Kod='EUR']/BanknoteSelling").InnerXml;
+                    DGridKurlar.Rows.Add("Dolar", USD);
 
-            string GBP = xmlDoc.SelectSingleNode("Tarih_Date/Currency[@Kod='GBP']/BanknoteSelling").InnerXml;
+                    dolarState = true;
+                }
+                else if (CBoxKurTuru.SelectedItem.ToString() == "Euro" && !euroState)
+                {
+                    string EUR = xmlDoc.SelectSingleNode("Tarih_Date/Currency[@Kod='EUR']/BanknoteSelling").InnerXml;
 
-            DGridKurlar.Rows.Add("Dolar", USD);
-            DGridKurlar.Rows.Add("Euro", EUR);
-            DGridKurlar.Rows.Add("Pound", GBP);
+                    DGridKurlar.Rows.Add("Euro", EUR);
+
+                    euroState = true;
+                }
+                else if (CBoxKurTuru.SelectedItem.ToString() == "Pound" && !poundState)
+                {
+                    string GBP = xmlDoc.SelectSingleNode("Tarih_Date/Currency[@Kod='GBP']/BanknoteSelling").InnerXml;
+
+                    DGridKurlar.Rows.Add("Pound", GBP);
+
+                    poundState = true;
+                }
+            }
+            #endregion
+        }
+
+        private void SelectedRow(string paraBirimi)
+        {
+            for (int i = 0; i < DGridKurlar.RowCount - 1; i++)
+            {
+                if (DGridKurlar.Rows[i].Cells[0].Value.ToString() == paraBirimi)
+                {
+                    DGridKurlar.Rows[i].Selected = true;
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            CBoxKurTuru.Items.Add("Dolar");
+            CBoxKurTuru.Items.Add("Euro");
+            CBoxKurTuru.Items.Add("Pound");
+            DGridTemizle();
+        }
+
+        private void CboxSelected(object sender, EventArgs e)
+        {
+            DGridTemizle();
+
+            if (CBoxKurTuru.SelectedItem.ToString() == "Dolar")
+            {
+                SelectedRow("Dolar");
+            }
+            else if (CBoxKurTuru.SelectedItem.ToString() == "Euro")
+            {
+                SelectedRow("Euro");
+            }
+            else if (CBoxKurTuru.SelectedItem.ToString() == "Pound")
+            {
+                SelectedRow("Pound");
+            }
+        }
+
+        public void DGridTemizle()
+        {
+            for (int i = 0; i < DGridKurlar.RowCount - 1; i++)
+            {
+                DGridKurlar.Rows[i].Selected = false;
+            }
         }
     }
 }
